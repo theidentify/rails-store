943f3edda46c07b6eb146e6757ac644d289fb78e
KrisanathepB <KrisanathepB@bedrockanalytics.ai>
2025-08-31 20:52:36 +0700

    Initial commit

---
+++ app/controllers/unsubscribes_controller.rb
@@ -0,0 +1,17 @@
+class UnsubscribesController < ApplicationController
+  allow_unauthenticated_access
+  before_action :set_subscriber
+
+  def show
+    puts "hello"
+    @subscriber&.destroy
+    redirect_to root_path, notice: "Unsubscribed successfully."
+  end
+
+
+  private
+
+  def set_subscriber
+    @subscriber = Subscriber.find_by_token_for(:unsubscribe, params[:token])
+  end
+end
